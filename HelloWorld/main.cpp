33c8c9b28873ac4a4544c3715dc38a8c5b97da51
irrokez <vasilevstas.forgit@bk.ru>
2019-07-01 10:31:51 +0300

    Add test folder

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "I changed programm! " <<endl;
+    cout << "I changed programm secondly. " <<endl;
+    return 0;
+}
